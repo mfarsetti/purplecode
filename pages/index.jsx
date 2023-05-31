a4c73a6fd6ceec3a49fd10b27fe718243e5d2056
Matheus Farsetti <matheus.farsetti@gmail.com>
2023-05-30 23:10:16 -0300

    feat: sync home

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ export default class Index extends Component {
                       <p>Estamos em processo de manutenção para<br /> aprimorar sua experiência em nosso site.</p>
                       <p><strong>Em breve estaremos no ar com uma cara<br /> nova e cheios de novidades incríveis.</strong></p>
                       <p>Enquanto isso, se precisar falar com <br /> a Purple Code, é só clicar aqui embaixo.</p>
-                      <a href="#">Quero falar com a Purple</a>
+                      <a href="https://wa.link/c3x5jw" target="_blank">Quero falar com a Purple</a>
 
                   </div>
 
